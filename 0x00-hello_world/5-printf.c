6f308658f343f8b8f7c4691fe5dd42de836790fb
Educhebii <chebonedwinchebii@gmail.com>
2023-03-20 10:01:09 -0700

    updates on 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
 *main - Entry point
 *
-*Return: Always 0(Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece od art.\n");
-	return(0);
+	printf("with proper grammer, but the outcome is a piece od art. \n");
+	return (0);
 }
 
